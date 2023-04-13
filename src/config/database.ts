11f58d205ae1e382ece2c0c29b8b82f8308a80dd
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-04-13 12:21:56 -0300

    feat: get cars and car

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,3 @@
-// import pg from "pg";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const { Pool } = pg;
-// const configDatabase = {
-//   connectionString: process.env.DATABASE_URL
-// };
-
-// const db = new Pool(configDatabase);
-// export default db;
-
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
