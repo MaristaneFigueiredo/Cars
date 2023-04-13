85f2c8ca1954c89de40bce3f9ea6f8f72fb88123
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-04-13 12:13:48 -0300

    feat: installation and cofiguration prisma

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,17 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const { Pool } = pg;
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL
-};
+// const { Pool } = pg;
+// const configDatabase = {
+//   connectionString: process.env.DATABASE_URL
+// };
 
-const db = new Pool(configDatabase);
-export default db;
+// const db = new Pool(configDatabase);
+// export default db;
+
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default prisma
